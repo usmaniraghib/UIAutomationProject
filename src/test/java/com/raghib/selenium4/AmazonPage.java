bbc6be49b2a456c674d96917df5a5d66872c6368
usmaniraghib <javaprogrammers786@gmail.com>
2023-07-28 14:42:41 +0530

    ok

--- src/test/java/com/raghib/selenium4/AmazonPage.java
+++ src/test/java/com/raghib/selenium4/AmazonPage.java
@@ -1,6 +1,5 @@
 package com.raghib.selenium4;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
